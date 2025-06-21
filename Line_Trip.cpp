fc2934706fbfdc762abc8600333861ab9a610899
Aliya Aftab <aftabaliya40@gmail.com>
2025-06-21 19:19:21 +0530

    updated solution

--- Line_Trip.cpp
+++ Line_Trip.cpp
@@ -5,12 +5,29 @@ int main(){
 int t;
 cin>>t;
 while(t--){
-int n,x;
+long long n,x;
 cin>>n>>x;
-vector<int>a(n);
+vector<int>a;
+a.push_back(0);
 for(int i=0;i<n;i++){
-cin>>a[i];
+long long point;
+cin>>point;
+a.push_back(point);
 }
+a.push_back(x);
+n=a.size();
+int maxDist = INT_MIN;
+        for (int i = 1; i < n; i++) {
+            if(i==n-1){
+            maxDist = max(maxDist, 2*(a[i] - a[i - 1]));
+            }
+            else{
+            maxDist = max(maxDist, a[i] - a[i - 1]);
+            }
 
-}    
+        }
+
+        cout << maxDist << endl;
+    }
+
 }
